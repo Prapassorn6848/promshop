b79e3dbb5eeb578f29b63ff8594b117a23bebce4
Proramaporn <64032743+Proramaporn@users.noreply.github.com>
2021-10-08 21:01:34 +0700

    proramaporn

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -10,10 +11,47 @@ class Login extends Component {
 
     render() {
         return ( 
-            <div style={{width:"100%",height:"100vh",backgroundColor:"#29292B"}}>
-                <div style={{Width:"50%",fontSize:'120px',color:"#FFB636",textAlign:'center',borderWidth:'bold'}}>
-                    Hello world
+            <div style={{direction:'row',width:"100%",height:"100vh",backgroundColor:"#29292B"}}>
+                <div style={{marginLeft:"50%",direction:'column',height:"100vh"}}>
+                    <div style={{textAlign:'center',justifyContent:"center",alignItems:"center"}}>
+                        <div style={{height:"10vh"}}></div>
+                        <a1 style={{color:"#FFB636",fontSize:"50px"}}>PROMSHOP</a1>
+                        <div style={{height:"1.5vh"}}></div>
+                        <a1 style={{color:"#FFB636",fontSize:"20px"}}>Enter your username or email address and password</a1>
+                        <div style={{height:"0.1vh"}}></div>
+                        <a1 style={{color:"#FFB636",fontSize:"20px"}}>to get access account</a1>
+                        <div style={{height:"5vh"}}></div>
+                    </div>
+                    <div style={{alignItems:"center"}}>
+                        <div style={{marginLeft:'17%'}}>
+                            <a1 style={{color:"#FFB636",fontSize:"20px"}} type="text">Username or E-mail</a1>
+                        </div>
+                        <div style={{textAlign:'center'}}>
+                            <input style={{ marginTop:'10px',width: 500,height:40,color:"black"}} type="text" name="email"/>
+                        </div>
+                        <div style={{marginLeft:'17%',marginTop:'2%'}}>
+                            <a1 style={{color:"#FFB636",fontSize:"20px"}} type="text">Password</a1>
+                        </div>
+                        <div style={{textAlign:'center'}} >
+                            <input style={{ marginTop:'10px',width: 500,height:40,color:"black"}} type="text" name="email"/>
+                        </div>
+                    </div>
+                    <div style={{marginLeft:'60%'}}>
+                        <a1 style={{color:"#FF0000",fontSize:"20px"}}>forgotten password ?</a1>
+                    </div>
+                    <div style={{textAlign:'center',marginTop:'10%'}}>
+                        <button style={{ width: 200,height:50,borderRadius:'40px',fontSize:'30px',fontWeight:'bold',fontFamily:"initial",color:'#FFB636',cursor: 'pointer'}}>
+                            Login
+                        </button>
+                    </div>
+                    <div style={{textAlign:'center',marginTop:'3%'}}>
+                        <button style={{ width: 300,height:50,borderRadius:'40px',fontSize:'30px',fontWeight:'bold',fontFamily:"initial",color:"#FFB636",cursor: 'pointer'}}>
+                            Create New Account
+                        </button>
+                    </div>
                 </div>
+                
+                
             </div>
         )
     }
