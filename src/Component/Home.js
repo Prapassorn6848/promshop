aca0d10e70f3bc0ce2c3ee3be2841f8a3c9137d8
cherwet0 <92036868+cherwet0@users.noreply.github.com>
2021-10-10 05:19:08 +0700

    Add files via upload

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import history from '../history'
 import firestore from "../firebase/firestore"
+import { ButtonGroup } from 'react-bootstrap';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    NavLink,
+    Route,
+    Link
+  } from "react-router-dom";
+import './HomeS.css';
 
 class Home extends Component {
     constructor(props) {
@@ -11,8 +21,23 @@ class Home extends Component {
     }
 
     render() {
+        
         return (
-            <div></div>
+            <div><h1 class="top ">CS SHOP</h1>
+                <div class="text-box">
+                <a href="#"  class="btn btn btn-white btn-animate " >Stock</a>
+                </div>
+
+                <div class="text-box1">
+                <a href="/Edit" class="btn btn-white btn-animate " >Edit</a>
+                </div>
+
+                <div class="text-box2">
+                <a href="/History" class="btn btn-white btn-animate " >History</a>
+                </div>
+                
+            </div>
+            
         )
     }
 }
