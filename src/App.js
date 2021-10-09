fcbd43828ab2a9ca9d7c6702f455df810668fb4b
Ferinajang <fern179@hotmail.co.th>
2021-10-09 16:38:39 +0700

    update table view
    from nongfern consuay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch, Router } from 'react-router-dom';
 import React, { Component } from 'react';
 import history from './history';
 import Login from './Component/Login';
+import View from './Component/View';
 
 
 class App extends Component {
@@ -17,7 +18,7 @@ class App extends Component {
 
         <Router history={history}>
           <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path="/" component={View} />
           </Switch>
         </Router>
 
--- src/Component/View.js
+++ src/Component/View.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react'
 import firebase from 'firebase';
 import firestore from "../firebase/firestore"
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './Style.css';
+import { DataGrid } from '@mui/x-data-grid';
 
 //const db = firebase.firestore();
 console.log("fern");
 
-
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'age',headerName: 'Age',type: 'number',width: 90,},
+    { field: 'fullName',headerName: 'Full name',description: 'This column has a value getter and is not sortable.',sortable: false,width: 160,
+      valueGetter: (params) =>
+        `${params.getValue(params.id, 'firstName') || ''} ${
+          params.getValue(params.id, 'lastName') || ''
+        }`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
 class View extends Component {
     constructor(props) {
@@ -26,55 +50,30 @@ class View extends Component {
 
     render() {
         return (
-            <div style={bg}>
-            <h1 style={cssStyle}> Promshop </h1>
-            <table  class="table table table-hover table-dark "style={tb}>
-              <thead class="bg-warning">
-                <tr style={cssStyle}>
-                  <th>ชื่อ</th>
-                  <th>นามสกุล</th>
-                  <th>อีเมล</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr style={cssStyle}>
-                  <td>นิสากร</td>
-                  <td>โครตสวย</td>
-                  <td>สวยเกิ๊น</td>
-                </tr>
-                <tr style={cssStyle}>
-                  <td>นังมิว</td>
-                  <td>ไม่สวย</td>
-                  <td>เบอรรี่</td>
-                </tr>
-                <tr style={cssStyle}>
-                  <td>นังพิง</td>
-                  <td>นังงูพิก</td>
-                  <td>เดี๋ยวโดนฉก</td>
-                </tr>
-              </tbody>
-            </table>
+            <div id="container">
+                <div class = "view1">
+                <div style={{ width: '100%' ,height:'5vh' , backgroundColor:'#29292B'}} ></div>
+                <div class='view' >
+                    <h1 style={{color:'#FFB636'}}> PROMSHOP</h1> 
+                </div>
+                <div style={{ width: '100%' ,height:'50vh'}}>
+                <DataGrid className='cssStyle'
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[1]}
+                />
+                </div>
+                <div class='view' ></div>
+                <div style={{ width: '100%' ,height:'10vh' , backgroundColor:'#FFB636'}} ></div>
+                </div>
+                </div>
+                
+                
             
-            </div>
  
         )
     }
-    
-}
-const cssStyle = {
-    color: '#FFB636', 
-    textAlign: 'center'
-}
-const bg = {
-    backgroundColor: '#29292B', 
-    widht : '100%',
-    height:'100vh',
-}
-const tb = {
-    justifyContent : "center",
-    margin: '5%',
-    width : "70%",
-    height:'30vh',
     
 }
 
