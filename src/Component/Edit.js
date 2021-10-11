9b38ddd3758d38a2607a3053f88fa6cc9f81fc0f
Proramaporn <64032743+Proramaporn@users.noreply.github.com>
2021-10-12 01:58:41 +0700

    edit

--- src/Component/Edit.js
+++ src/Component/Edit.js
@@ -4,10 +4,11 @@ import './EditStyle.css';
 import { DataGrid } from '@mui/x-data-grid';
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { Button, Link } from "@material-ui/core";
-import {FiLogOut} from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
 import { connect } from 'react-redux';
-import { addUser , editUser} from '../actions/userAction';
-import { addProduct , deleteProduct} from '../actions/productAction';
+import { addUser, editUser } from '../actions/userAction';
+import { addProduct, deleteProduct } from '../actions/productAction';
+import firestore from "../firebase/firestore"
 /* 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -16,8 +17,8 @@ const useStyles = makeStyles((theme) =>
 ); */
 const handleClick = (event, cellValues) => {
     console.log(cellValues.row);
-    
-  };
+
+};
 /* const handleCellClick = (param, event) => {
     event.stopPropagation();
 };
@@ -25,62 +26,77 @@ const handleRowClick = (param, event) => {
     event.stopPropagation();
 }; */
 const columns = [
-    {field: "name",headerName: "Name",width: 250},
-    { field: "price",headerName: "Price", width: 250 },
-    {field: "quantity",headerName: "quantity",width: 250},
+    { field: 'productID', headerName: 'ID', width: 200 },
+    { field: 'name', headerName: 'Name', width: 250},
+    { field: 'price', headerName: 'Price',type: 'number', width: 205 },
+    { field: 'QTY',headerName: 'QTY',type: 'number',width: 205},
     {
-      field: "Delete",width:250,renderCell: (cellValues) => {
-        return (
-          <Button variant="contained" color="secondary" onClick={(event) => {handleClick(event, cellValues);}}>Delete</Button>
-        );
-      }
+        field: "Delete", width: 250, renderCell: (cellValues) => {
+            return (
+                <Button variant="contained" color="secondary" onClick={(event) => { handleClick(event, cellValues); }}>Delete</Button>
+            );
+        }
     },
- ];
- const rows  = [
-    { id: 1,name: 'tV',price: '200',quantity: '2',},
-    { id: 2,name: 'airpod',price: '5000',quantity: '10'},
-    { id: 3,name: 'ipad',price: '12000',quantity: '5'},
-    { id: 4,name: 'nokia',price: '1000',quantity: '30'},
-    { id: 5,name: 'sumsung',price: '100000',quantity: '100'},
-    { id: 6,name: 'computer',price: '25000',quantity: '9'},
 ];
+
 class Edit extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            productList:[],
         };
     }
 
+    componentDidMount() {
+        firestore.getAllProduct(this.getAllProductSuccess, this.getAllReject)
+
+    }
+
+    getAllProductSuccess = (querySnapshot) => {
+        querySnapshot.forEach(doc => {
+            let product = doc.data()
+            product.id = doc.id
+            this.setState({ productList: this.state.productList.concat(product) })
+            console.log(this.state.productList)
+            console.log(product)
+        });
+        /*console.log(this.props.productList)*/
+
+
+    }
+
+    getAllReject = (error) => {
+        console.log(error)
+    }
     render() {
         return (
             <div id="container">
-                <div class = "view1">
-                <div style={{ width: '100%' ,height:'5vh' , backgroundColor:'#29292B'}} ></div>
-                <div class='view' >
-                    <h1 style={{color:'#FFB636'}}> PROMSHOP</h1>
-                    <h5 style={{color:'#FFB636'}}> Editor</h5> 
-                    <h1 class="tester">Mariarty
-                    <div style={{marginLeft:'85%',marginTop:'-14%'}}>
-                     <FiLogOut size={40} onClick={()=>window.location.href="/"} style={{cursor:'pointer'}}/>           
-                </div>
+                <div class="view1">
+                    <div style={{ width: '100%', height: '5vh', backgroundColor: '#29292B' }} ></div>
+                    <div class='view' >
+                        <h1 style={{ color: '#FFB636' }}> PROMSHOP</h1>
+                        <h5 style={{ color: '#FFB636' }}> Editor</h5>
+                        <h1 class="tester">Mariarty
+                            <div style={{ marginLeft: '85%', marginTop: '-14%' }}>
+                                <FiLogOut size={40} onClick={() => window.location.href = "/"} style={{ cursor: 'pointer' }} />
+                            </div>
 
-                </h1>
-                <h4  class="tester2"> Admin</h4> 
-                </div>
-                <div  style={{ width: '100%' ,height:'50vh'}}>
-                <DataGrid className='cssStyle'
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[1]}
-                    
-                />
-                <Button  class="button1" onClick>Back</Button>
-                </div>
-                <div class='view' ></div>
-                <div style={{ width: '100%' ,height:'5vh' , backgroundColor:'#FFB636'}} ></div>
-                
+                        </h1>
+                        <h4 class="tester2"> Admin</h4>
+                    </div>
+                    <div style={{ width: '100%', height: '50vh' }}>
+                        <DataGrid className='cssStyle'
+                            rows={this.state.productList}
+                            columns={columns}
+                            pageSize={5}
+                            rowsPerPageOptions={[1]}
+
+                        />
+                        
+                    </div>
+                    <div class='view' ></div>
+                    <div style={{ width: '100%', height: '5vh', backgroundColor: '#FFB636' }} ></div>
+
                 </div>
             </div>
         )
@@ -94,14 +110,14 @@ const mapDispatchToProps = (dispatch) => {
         addUser: (user) => dispatch(addUser(user)),
         addProduct: (product) => dispatch(addProduct(product)),
         editUser: (user) => dispatch(editUser(user)),
-        deleteProduct:(product) => dispatch(deleteProduct(product))
+        deleteProduct: (product) => dispatch(deleteProduct(product))
     };
 };
 
 const mapStateToProps = (state) => {
     return {
         userList: state.userReducer.userList,
-        products: state.productReducer.products,
+        productList: state.productReducer.productList,
     };
 };
 
