021f082809c0e7880b4da15913bea0dc91f57202
Proramaporn <64032743+Proramaporn@users.noreply.github.com>
2021-10-11 22:00:51 +0700

    redux

---
+++ src/reducers/productReducer.js
@@ -0,0 +1,22 @@
+import { ADD_PRODUCT,DELETE_PRODUCT } from "../actions/types";
+
+const initialState = {
+    products:[]
+};
+
+const productReducer = (state = initialState,action) => {
+    switch(action.type){
+        case ADD_PRODUCT : 
+            return{
+                ...state,
+                products:state.products.concat(action.product)
+            }
+        case DELETE_PRODUCT :
+            return{
+                ...state,
+                products:state.products.filter((item)=>item.id != action.id)
+            }
+        default : return{state}
+    }
+}
+export default productReducer;
